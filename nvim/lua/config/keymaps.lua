60a783920d82c8c07973aceda582e90647e34401
Moisés Sales <92830922+moises-sls@users.noreply.github.com>
2025-09-16 01:07:00 -0300

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,12 @@
-vim.keymap.set('n', '<leader>h', '<cmd>nohlsearch<CR>', { desc = "No Highlight Search" })
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, {desc = ("Previous page") })
+vim.g.mapleader = " "
+vim.g.maplocalleader = '\\'
 
-vim.keymap.set('n', '<leader>ww', '<cmd>w<CR>', { desc = "Save file" })
+vim.keymap.set('n', '<leader>h', ":nohlsearch<CR>", { desc = "No Highlight Search" })
 
-vim.keymap.set('n', '<leader>qq', '<cmd>q!<CR>', { desc =  "Quit without saving" })
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = "Previous page" })
 
-vim.keymap.set('n', '<leader>wq', '<cmd>wq<CR>', { desc = "Save and Quit" })
+vim.keymap.set('n', '<leader>wq', ":wq<CR>", { desc = "Write and Quit" })
 
-vim.keymap.set('n', '<leader>qa', '<cmd>qa<CR>', { desc = "Quit all" })
+vim.keymap.set('n', '<leader>ww', ":w<CR>", { desc = "Write" })
 
-
+vim.keymap.set('n', '<leader>qq', ":q!<CR>", { desc = "Quit without saving "})
